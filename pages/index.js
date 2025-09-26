d5501a20175aed77a40a4ed450bf96986f5c6928
lucyomgggg <166032608+lucyomgggg@users.noreply.github.com>
2025-09-26 23:48:14 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,9 @@
+import dynamic from "next/dynamic";
 
+
+const MapView = dynamic(() => import("../components/MapView"), { ssr: false });
+
+
+export default function Home() {
+return <MapView />;
+}
